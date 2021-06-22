80e28dada93a55b29423e8c98383da301edf4769
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-06-22 23:22:44 +0530

    Add files via upload

---
+++ print all subarrays.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printAllSubarrays(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j <n ; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    printAllSubarrays(arr, n);
+    return 0;
+}
